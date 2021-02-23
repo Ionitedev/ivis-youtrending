4a5a63cb39ba799c3c43bfa835329eb1720606db
Ionite <406408224@qq.com>
2021-02-23 15:25:50 +0100

    index page sketch

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,49 @@
 from flask import Flask
-import csv
 import pickle
 
-with open('app/static/Self-Introduction-to-IVIS21-_Responses_.csv') as f:
-    csv_data = tuple(csv.reader(f))
+# read
+full_data = {}
+country_index = {}
+lang_index = {}
+time_index = {}
+category_index = {}
+view_rank = ()
+category_view_rank = {}
 
-s = b''
-for i in range(11):
-    with open('app/static/{}.pkl'.format(i), 'rb') as f:
-        s += f.read()
-
-test_data = pickle.loads(s)
-
-for i, line in enumerate(csv_data):
-    line.append(i)
-    if i == 0:
-        continue
-    for j in range(12, 24):
-        line[j] = int(line[j])
+category_names = {  # 34 -> 23 when reading a record of video
+    '1': 'Film & Animation',
+    '2': 'Autos & Vehicles',
+    '10': 'Music',
+    '15': 'Pets & Animals',
+    '17': 'Sports',
+    '18': 'Short Movies',
+    '19': 'Travel & Events',
+    '20': 'Gaming',
+    '21': 'Videoblogging',
+    '22': 'People & Blogs',
+    '23': 'Comedy',
+    '24': 'Entertainment',
+    '25': 'News & Politics',
+    '26': 'Howto & Style',
+    '27': 'Education',
+    '28': 'Science & Technology',
+    '29': 'Nonprofits & Activism',
+    '30': 'Movies',
+    '31': 'Anime/Animation',
+    '32': 'Action/Adventure',
+    '33': 'Classics',
+    '34': 'Comedy',
+    '35': 'Documentary',
+    '36': 'Drama',
+    '37': 'Family',
+    '38': 'Foreign',
+    '39': 'Horror',
+    '40': 'Sci-Fi/Fantasy',
+    '41': 'Thriller',
+    '42': 'Shorts',
+    '43': 'Shows',
+    '44': 'Trailers'
+}
 
 app = Flask(__name__)
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,41 +1,37 @@
-from app import app, csv_data, group_assessment, test_data
+from app import app, full_data, country_index, lang_index, time_index, category_index, view_rank, category_view_rank, category_names
 from flask import render_template, request, session, redirect
+from functools import reduce
 
-@app.route('/')
-def test():
-    return str(test_data[10000:100010])
-
-@app.route('/index', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index_page():
-    # default page
     if request.method == 'GET':
-        return render_template('index.html', data=csv_data, attr_checked=range(12, 24), entity_checked=[])
-    
-    # ordered page
-    entity_checked = list(map(int, request.form.getlist('member')))
-    attr_checked = list(map(int, request.form.getlist('skill')))
-    data = csv_data
-    if 'order' in request.form:
-        data = [data[0]] + sorted(data[1:], key=lambda x: int(x[int(request.form['order'])]), reverse=True)
-        return render_template('index.html', data=data, attr_checked=attr_checked, entity_checked=entity_checked)
+        selected = ['all', 'all', 'all']
     else:
-        return redirect('group', code=307)
+        selected = [request.form['time'], request.form['country'], request.form['lang']]
+        
+    selected_index = set(full_data.keys())
+    try:
+        # filter by time
+        if selected[0] != 'all':
+            selected_index = selected_index.intersection(time_index[selected[0]])
 
-@app.route('/person', methods=['GET'])
-def person_page():
-    id = request.args.get('id')
-    return render_template('person.html', data=csv_data[int(id)], questions=csv_data[0])
+        # filter by country
+        if selected[1] != 'all':
+            selected_index = selected_index.intersection(country_index[selected[1]])
 
-@app.route('/group', methods=['POST'])
-def group():
-    members = list(map(int, request.form.getlist('member')))
-    skills = list(map(int, request.form.getlist('skill')))
+        # filter by lang
+        if selected[2] != 'all':
+            selected_index = selected_index.intersection(lang_index[selected[2]])
 
-    if len(members) < 4 or len(members) > 5:
-        return '<h2>You should choose 4 or 5 group members</h2>'
+        # classify by category
+        selected_category = [x for x in ((i, selected_index.intersection(category_index[i])) for i in sorted(category_index.keys())) if len(x[1]) > 0]
+        selected_category_top = {i[0]: sorted(i[1], key=category_view_rank[i[0]].index, reverse=True)[:3] for i in selected_category}
+    except Exception:
+        pass
+        
+    return render_template('index.html', selected=selected)
 
-    if len(skills) < 1:
-        return '<h2>You should choose at least one skill</h2>'
-
-    assess, scp = group_assessment.assessment(members, skills)
-    return render_template('group.html', assess=assess, members=[(csv_data[i][1], i) for i in members], scp=scp, recommended=max(scp, key=scp.get))
+@app.route('/category', methods=['POST'])
+def category_page():
+    selected = [request.form['time'], request.form['country'], request.form['lang']]
+    return render_template('index.html', selected=selected)
